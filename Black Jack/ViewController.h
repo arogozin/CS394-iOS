eec353d3c8d83a131282879c3c6b260a21696fd9
Aleksandr Rogozin <aleksandr@arg-mpro.local>
2014-09-20 17:44:49 -0400

    Initial Commit

---
+++ Black Jack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Black Jack
+//
+//  Created by Aleksandr Rogozin on 9/20/14.
+//  Copyright (c) 2014 Rogozin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Black JackTests/Black_JackTests.m
@@ -0,0 +1,39 @@
+//
+//  Black_JackTests.m
+//  Black JackTests
+//
+//  Created by Aleksandr Rogozin on 9/20/14.
+//  Copyright (c) 2014 Rogozin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Black_JackTests : XCTestCase
+
+@end
+
+@implementation Black_JackTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
